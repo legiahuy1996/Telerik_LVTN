b03cf2ae8f1755e8a02b4a93202fb02ad9495f6d
Huy Cute <kodcquennghen@outlook.com>
2018-05-11 19:52:19 +0700

    edit site master

--- TelerikWebApp1/Site1.Master.designer.cs
+++ TelerikWebApp1/Site1.Master.designer.cs
@@ -20,5 +20,14 @@ namespace TelerikWebApp1 {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder content;
+        
+        /// <summary>
+        /// script control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder script;
     }
 }
