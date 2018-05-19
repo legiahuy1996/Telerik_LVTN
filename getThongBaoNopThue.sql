ceb87403e22a14c0e23c115403c70eb88305ed1b
Phong <Phong@DESKTOP-SAM8MKA>
2018-05-19 12:41:11 +0700

    store ke khai nop thue

---
+++ getThongBaoNopThue.sql
@@ -0,0 +1,11 @@
+ALTER PROC getThongBaoNopThue
+@thang	NVARCHAR(7) = NULL
+AS
+DECLARE @dThang int,@dNam	VARCHAR(4)
+SET @dThang = LEFT(@thang,2)
+SET @dNam = RIGHT(@thang,4)
+
+SELECT a.KyThue,a.idKhaiThue,a.ThueGTGN,a.ThueTNCN,a.ThueMonBai,a.SoNo,a.TongCong 
+FROM dbo.ThongBaoNopThue (NOLOCK) a
+LEFT JOIN dbo.KhaiThue (NOLOCK) b ON b.idKhaiThue = a.idKhaiThue
+WHERE a.KyThue = @dThang AND b.nam = @dNam
